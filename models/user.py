1547edd522fc5a7f2993461e490c47027d4ecd8c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-19 11:42:21 -0800

    Task 7

--- models/user.py
+++ models/user.py
@@ -18,3 +18,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the User."""
+        super().__init__(*args, **kwargs)
