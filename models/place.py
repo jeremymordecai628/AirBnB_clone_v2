f6edd1fcb7452f6f4e246960abf46a3171bc88ff
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-19 12:34:03 -0800

    Task 9

--- models/place.py
+++ models/place.py
@@ -2,14 +2,14 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import ForeignKey
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
 
-    if storage_type = "db":
+    if models.storage_type = "db":
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -20,4 +20,27 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=False)
         longitude = Column(Float, nullable=False)
+        reviews = relationship('Review'
+                cascade='all, delete',
+                backref='place')
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
         amenity_ids = []
+
+        @property
+        def reviews(self):
+            """Getter attribute that returns the list of Review instance"""
+            reviews_list = []
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id
+                reviews_list.append(review)
+            return reviews_list
