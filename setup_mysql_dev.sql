c2f878c3c52e46d7bd56c085e74bc9b5662006d4
kaymacharia <kaymachariaa@gmail.com>
2024-02-19 04:15:57 -0800

    1

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,13 @@
--- Creates the database hbnb_dev_db with specified paramenters
+-- Generates the hbnb_dev_db database with defined parameters.
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Creates user if doesn't exist
+-- Creates a user in case it does not already exist.
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
--- Sets password for user
+-- Assigns a password to a user.
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
--- Grants privileges to user on database
+-- Bestows privileges to a user for a specific database.
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grants select privileges to user on performance_schema database
+-- Provides the user with SELECT privileges on the performance_schema database.
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- Flush privileges
 FLUSH PRIVILEGES;
