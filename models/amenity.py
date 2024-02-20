a71827b2e6312ad8eba433c2c5439fd00b373a76
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-20 12:50:26 -0800

    Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = realtionship(
