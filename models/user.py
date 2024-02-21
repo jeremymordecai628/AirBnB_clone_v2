70ff83bdb33f71d42270cbf91f738ca0def3b83a
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-21 12:33:38 -0800

    10

--- models/user.py
+++ models/user.py
@@ -6,17 +6,19 @@ from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from models.base_model import Base
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship("Place",
-            cascade="all, delete",
-            backref="user")
-    reviews = relationship("Review", cascade="all, delete",
-            backref="user")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place",
+                cascade="all, delete",
+                backref="user")
+        reviews = relationship("Review", cascade="all, delete",
+                backref="user")
