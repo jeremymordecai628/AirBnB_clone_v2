e7ec75aeacbf77d1a77918e0f521c177d0e7a6e2
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-21 08:28:27 -0800

    removing imports

--- models/user.py
+++ models/user.py
@@ -10,15 +10,14 @@ from models.base_model import Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_type == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship('Place',
-                cascade='all, delete',
-                backref='user')
-        reviews = relationship('Review',
-                cascade='all, delete',
-                backref='user')
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship('Place',
+            cascade='all, delete',
+            backref='user')
+    reviews = relationship('Review',
+            cascade='all, delete',
+            backref='user')
