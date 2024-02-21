e1f346cfa36e724e742894335c16feb1f2daae32
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-21 08:04:39 -0800

    reverting

--- models/user.py
+++ models/user.py
@@ -10,15 +10,15 @@ from models.base_model import Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship('Place',
-            cascade='all, delete',
-            backref='user')
-    reviews = relationship('Review',
-            cascade='all, delete',
-            backref='user')
+    if models.storage_type == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place',
+                cascade='all, delete',
+                backref='user')
+        reviews = relationship('Review',
+                cascade='all, delete',
+                backref='user')
