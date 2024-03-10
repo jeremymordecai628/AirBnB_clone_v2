fb156efbbed67e8c36b1049679641bf8d031a179
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-10 07:47:58 -0700

    3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,13 +38,13 @@ def do_deploy(archive_path):
         filename = os.path.basename(archive_path)
         archive_folder = "/data/web_static/releases/{}".format(
                                                         filename.split(".")[0])
-        run('mkdir -p {}/'.format(archive_folder))
-        run('tar -xzf /tmp/{} -C {}/'.format(filename, archive_folder))
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(filename, archive_folder))
         run('rm /tmp/{}'.format(filename))
         run('mv {}/web_static/* {}/'.format(archive_folder, archive_folder))
         run('rm -rf {}/web_static'.format(archive_folder))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(archive_folder))
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
         print("New version deployed!")
         return True
     except Exception as e:
