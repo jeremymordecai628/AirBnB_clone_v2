67e3bacdb04144e3a7019e4e5d6cda323f892e45
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-10 13:24:09 -0700

    5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,40 +1,62 @@
 # Install Nginx package
 package { 'nginx':
   ensure => installed,
-}
+} ->
 
 # Create directory structure
 file { '/data':
   ensure => 'directory',
-}
+} ->
 
 file { '/data/web_static':
   ensure => 'directory',
-}
+} ->
 
 file { '/data/web_static/releases':
   ensure => 'directory',
-}
+}->
 
 file { '/data/web_static/shared':
   ensure => 'directory',
-}
+} ->
 
 file { '/data/web_static/releases/test':
   ensure => 'directory',
-}
+} ->
 
 # Create a fake HTML file
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html><head></head><body>Holberton School</body></html>',
+  ensure  => 'present',
+  content => 'Holberton School Puppet\n',
 }
 
 # Create symbolic link
 file { '/data/web_static/current':
   ensure  => 'link',
   target  => '/data/web_static/releases/test',
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
 
 # Update Nginx configuration
 file { '/etc/nginx/sites-available/default':
@@ -57,4 +79,8 @@ server {
 service { 'nginx':
   ensure => running,
   enable => true,
+}
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
