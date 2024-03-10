584fd3239e09aabd434f1354b40eefc08669b773
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-10 07:36:08 -0700

    3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -56,6 +56,6 @@ def deploy():
     creates and distributes an archive to my website
     """
     archive_path = do_pack()
-    if archive_path is None:
+    if not archive_path:
         return False
     return do_deploy(archive_path)
