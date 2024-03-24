d4a4fa21ff80de81f3c665a4bf98ee717d6143de
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-24 21:49:01 +0300

    compiling

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
-    Returns a string 
+    Returns a string
     """
-	return 'Hello HBNB!'
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     """
     Ensures that the module is not executable when  imported
     """
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
