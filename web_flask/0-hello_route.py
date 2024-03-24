e0f0a57b44d90a0e31757ddbd617454bb1347bdf
root <root@DESKTOP-RB87F2G>
2024-03-24 21:38:44 +0300

    2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python3
+"""This script starts the Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Returns a string 
+    """
 	return 'Hello HBNB!'
 
 if __name__ == '__main__':
+    """
+    Ensures that the module is not executable when  imported
+    """
 	app.run(host='0.0.0.0', port=5000)
