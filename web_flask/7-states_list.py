a12ffb76457c2407c93ee87123ffe0cbefa5fa76
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-27 20:09:51 +0300

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This script starts the Flask web application"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a HTML page"""
+    states = sorted(storage.all("State").values(), key=lambda x: x.name)
+    return render__template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
